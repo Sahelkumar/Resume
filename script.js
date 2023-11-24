460289a8a77c4357d94de5f9b2628d1e87513e0e
Sahel Kumar Mondal <134678670+Sahelkumar@users.noreply.github.com>
2023-11-24 23:53:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function showAlert(){
+    var element = document.getElementById("message");
+    element.innerHTML="ok";
+    alert("are you sure!");
+}
+
+function sent(){
+    var sent = document.getElementById("message1")
+    sent.innerHTML = "message has been sent";
+}
+    
+    
+
+    
+        // Code to be executed when the button is clicked
+
+
